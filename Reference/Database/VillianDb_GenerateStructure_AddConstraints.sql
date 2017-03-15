dbb46caecaef336d51cba048d460f6e16f5b1469
Ben Holland <bholland@csumb.edu>
2017-03-15 19:23:04 +0000

    Added Some Test Data Definitions

--- Reference/Database/VillianDb_GenerateStructure_AddConstraints.sql
+++ Reference/Database/VillianDb_GenerateStructure_AddConstraints.sql
@@ -7,8 +7,16 @@
 -- Server version: 5.5.53-0ubuntu0.14.04.1
 -- PHP Version: 7.0.15-1+deb.sury.org~trusty+1
 
+
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
 --
 -- Constraints for dumped tables
 --
