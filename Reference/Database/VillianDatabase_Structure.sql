70581ccb20902269d3f85ef8b146534bc04c5382
Ben Holland <bholland@csumb.edu>
2017-03-15 17:45:16 +0000

    Added SQL script for generating the database structure without foreign keys.

--- Reference/Database/VillianDatabase_Structure.sql
+++ Reference/Database/VillianDatabase_Structure.sql
@@ -1,104 +1,102 @@
-﻿-- Exported from QuickDBD: https://www.quickdatatabasediagrams.com/
--- NOTE! If you have used non-SQL datatypes in your design, you will have to change these here.
 
-CREATE TABLE "Villian" (
-    "VillianId" int  NOT NULL ,
-    "Name" varchar(128) NOT NULL ,
-    "Address" varchar(1024)  NOT NULL ,
-    CONSTRAINT "pk_Villian" PRIMARY KEY (
-        "VillianId"
+CREATE TABLE Villian (
+    VillianId int  NOT NULL ,
+    Name varchar(128) NOT NULL ,
+    Address varchar(1024)  NOT NULL ,
+    CONSTRAINT pk_Villian PRIMARY KEY (
+        VillianId
     )
 )
 
 GO
 
-CREATE TABLE "Order" (
-    "OrderId" int  NOT NULL ,
-    "VillianIdId" int  NOT NULL ,
-    "OrderStatusId" int  NOT NULL ,
-    CONSTRAINT "pk_Order" PRIMARY KEY (
-        "OrderId"
+CREATE TABLE Order (
+    OrderId int  NOT NULL ,
+    VillianIdId int  NOT NULL ,
+    OrderStatusId int  NOT NULL ,
+    CONSTRAINT pk_Order PRIMARY KEY (
+        OrderId
     )
 )
 
 GO
 
-CREATE TABLE "OrderLine" (
-    "OrderLineId" int  NOT NULL ,
-    "OrderId" int  NOT NULL ,
-    "HenchpersonId" int  NOT NULL ,
-    CONSTRAINT "pk_OrderLine" PRIMARY KEY (
-        "OrderLineId"
+CREATE TABLE OrderLine (
+    OrderLineId int  NOT NULL ,
+    OrderId int  NOT NULL ,
+    HenchpersonId int  NOT NULL ,
+    CONSTRAINT pk_OrderLine PRIMARY KEY (
+        OrderLineId
     )
 )
 
 GO
 
-CREATE TABLE "Henchperson" (
-    "HenchpersonId" int  NOT NULL ,
-    "Name" varchar(200)  NOT NULL ,
-    "Description" varchar(1024)  NOT NULL ,
-    "RatePerHour" money  NOT NULL ,
-    "HenchersonStatusId" int  NOT NULL ,
-    "SpecialityId" int  NOT NULL ,
+CREATE TABLE Henchperson (
+    HenchpersonId int  NOT NULL ,
+    Name varchar(200)  NOT NULL ,
+    Description varchar(1024)  NOT NULL ,
+    RatePerHour money  NOT NULL ,
+    HenchersonStatusId int  NOT NULL ,
+    SpecialityId int  NOT NULL ,
     CONSTRAINT "pk_Henchperson" PRIMARY KEY (
-        "HenchpersonId"
+        HenchpersonId
     )
 )
 
 GO
 
-CREATE TABLE "Speciality" (
-    "SpecialityId" int  NOT NULL ,
-    "Description" varchar(1024)  NOT NULL ,
-    CONSTRAINT "pk_Speciality" PRIMARY KEY (
-        "SpecialityId"
+CREATE TABLE Speciality (
+    SpecialityId int  NOT NULL ,
+    Description varchar(1024)  NOT NULL ,
+    CONSTRAINT pk_Speciality PRIMARY KEY (
+        SpecialityId
     )
 )
 
 GO
 
-CREATE TABLE "HenchpersonStatus" (
-    "HenchpersonStatusId" int  NOT NULL ,
-    "Description" varchar(64)  NOT NULL ,
-    CONSTRAINT "pk_HenchpersonStatus" PRIMARY KEY (
-        "HenchpersonStatusId"
+CREATE TABLE HenchpersonStatus (
+    HenchpersonStatusId int  NOT NULL ,
+    Description varchar(64)  NOT NULL ,
+    CONSTRAINT pk_HenchpersonStatus PRIMARY KEY (
+        HenchpersonStatusId
     )
 )
 
 GO
 
-CREATE TABLE "OrderStatus" (
-    "OrderStatusID" int  NOT NULL ,
-    "Name" string  NOT NULL ,
-    CONSTRAINT "pk_OrderStatus" PRIMARY KEY (
-        "OrderStatusID"
+CREATE TABLE OrderStatus (
+    OrderStatusId int  NOT NULL ,
+    Name string  NOT NULL ,
+    CONSTRAINT pk_OrderStatus PRIMARY KEY (
+        OrderStatusID
     )
 )
 
 GO
 
-ALTER TABLE "Order" ADD CONSTRAINT "fk_Order_VillianIdId" FOREIGN KEY("VillianIdId")
-REFERENCES "Villian" ("VillianId")
+ALTER TABLE Order ADD CONSTRAINT fk_Order_VillianIdId FOREIGN KEY(VillianIdId)
+REFERENCES Villian (VillianId)
 GO
 
-ALTER TABLE "Order" ADD CONSTRAINT "fk_Order_OrderStatusId" FOREIGN KEY("OrderStatusId")
-REFERENCES "OrderStatus" ("OrderStatusID")
+ALTER TABLE Order ADD CONSTRAINT fk_Order_OrderStatusId FOREIGN KEY(OrderStatusId)
+REFERENCES OrderStatus (OrderStatusID)
 GO
 
-ALTER TABLE "OrderLine" ADD CONSTRAINT "fk_OrderLine_OrderId" FOREIGN KEY("OrderId")
-REFERENCES "Order" ("OrderId")
+ALTER TABLE OrderLine ADD CONSTRAINT fk_OrderLine_OrderId FOREIGN KEY(OrderId)
+REFERENCES Order (OrderId)
 GO
 
-ALTER TABLE "OrderLine" ADD CONSTRAINT "fk_OrderLine_HenchpersonId" FOREIGN KEY("HenchpersonId")
-REFERENCES "Henchperson" ("HenchpersonId")
+ALTER TABLE OrderLine ADD CONSTRAINT fk_OrderLine_HenchpersonId FOREIGN KEY(HenchpersonId)
+REFERENCES Henchperson (HenchpersonId)
 GO
 
-ALTER TABLE "Henchperson" ADD CONSTRAINT "fk_Henchperson_HenchersonStatusId" FOREIGN KEY("HenchersonStatusId")
-REFERENCES "HenchpersonStatus" ("HenchpersonStatusId")
+ALTER TABLE Henchperson ADD CONSTRAINT fk_Henchperson_HenchersonStatusId FOREIGN KEY(HenchersonStatusId)
+REFERENCES HenchpersonStatus (HenchpersonStatusId)
 GO
 
-ALTER TABLE "Henchperson" ADD CONSTRAINT "fk_Henchperson_SpecialityId" FOREIGN KEY("SpecialityId")
-REFERENCES "Speciality" ("SpecialityId")
+ALTER TABLE Henchperson ADD CONSTRAINT fk_Henchperson_SpecialityId FOREIGN KEY(SpecialityId)
+REFERENCES Speciality (SpecialityId)
 GO
 
---
+++ Reference/Database/VillianDb_GenerateStructure_NoFK.sql
@@ -0,0 +1,104 @@
+-- phpMyAdmin SQL Dump
+-- version 4.0.10deb1
+-- http://www.phpmyadmin.net
+--
+-- Host: 127.0.0.1
+-- Generation Time: Mar 15, 2017 at 05:39 PM
+-- Server version: 5.5.53-0ubuntu0.14.04.1
+-- PHP Version: 7.0.15-1+deb.sury.org~trusty+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "-08:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+
+--
+-- Database: `VillianDb`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Henchperson`
+--
+
+CREATE TABLE IF NOT EXISTS `Henchperson` (
+  `HenchpersonId` int(11) NOT NULL,
+  `Name` varchar(128) NOT NULL,
+  `Description` varchar(1024) NOT NULL,
+  `RatePerHour` decimal(10,0) NOT NULL,
+  `HenchpersonStatusId` int(11) NOT NULL,
+  `SpecialityId` int(11) NOT NULL,
+  PRIMARY KEY (`HenchpersonId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `HenchpersonStatus`
+--
+
+CREATE TABLE IF NOT EXISTS `HenchpersonStatus` (
+  `SpecialityId` int(11) NOT NULL,
+  `Description` varchar(1024) NOT NULL,
+  PRIMARY KEY (`SpecialityId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Order`
+--
+
+CREATE TABLE IF NOT EXISTS `Order` (
+  `OrderId` int(11) NOT NULL,
+  `VillianId` int(11) NOT NULL,
+  `OrderStatusId` int(11) NOT NULL,
+  PRIMARY KEY (`OrderId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `OrderLine`
+--
+
+CREATE TABLE IF NOT EXISTS `OrderLine` (
+  `OrderLineId` int(11) NOT NULL,
+  `OrderId` int(11) NOT NULL,
+  `HenchpersonId` int(11) NOT NULL,
+  PRIMARY KEY (`OrderLineId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `OrderStatus`
+--
+
+CREATE TABLE IF NOT EXISTS `OrderStatus` (
+  `OrderStatusId` int(11) NOT NULL,
+  `Description` varchar(1024) NOT NULL,
+  PRIMARY KEY (`OrderStatusId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `Villian`
+--
+
+CREATE TABLE IF NOT EXISTS `Villian` (
+  `VillianId` int(11) NOT NULL,
+  `Name` varchar(128) NOT NULL,
+  `Address` varchar(1024) NOT NULL,
+  PRIMARY KEY (`VillianId`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1;
+
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
