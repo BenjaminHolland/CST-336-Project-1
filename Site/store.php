ccf49fd4be5ebe86564826d9ec0b13a77dda7f37
Abraham Medina <abrahamedina24@gmail.com>
2017-03-25 19:03:15 +0000

    Added dataOutput file

--- Site/store.php
+++ Site/store.php
@@ -25,35 +25,35 @@
         ?>
         
         <form action = "<?php echo htmlspecialchars($_SERVER["PHP_SELF"]);?>" method = "POST" name = "henchmen">
-        <div class = "main">
-            <!--HEADER-->
-            <div class = "header">
-                <h1 class = "center">Henchmen Shop</h1>
-            </div>
-            <div class = "parentDiv">
-                <!--LEFT MENU-->
-                <div class = "sidebarContainer">
-                    <div class = "sidebar">
-                        <b>Filter by</b><br>
-                        <input type="radio" name="filter" value="a">a<br>
-                        <input type="radio" name="filter" value="b">b<br><br>
-                        
-                        <div class = "center"><input type = "submit" name = "filter" value = "Filter"></div>
+            <div class = "main">
+                <!--HEADER-->
+                <div class = "header">
+                    <h1 class = "center">Henchmen Shop</h1>
+                </div>
+                <div class = "parentDiv">
+                    <!--LEFT MENU-->
+                    <div class = "sidebarContainer">
+                        <div class = "sidebar">
+                            <b>Filter by</b><br>
+                            <input type="radio" name="filter" value="a">a<br>
+                            <input type="radio" name="filter" value="b">b<br><br>
+                            
+                            <div class = "center"><input type = "submit" name = "filter" value = "Filter"></div>
+                        </div>
+                    </div>
+                    <!--CONTENT-->
+                    <div class = "contentContainer">
+                        <div class = "content">
+                            Content...<br>
+                            <?php echo $_SESSION['output']; ?>
+                        </div>
                     </div>
                 </div>
-                <!--CONTENT-->
-                <div class = "contentContainer">
-                    <div class = "content">
-                        Content...<br>
-                        <?php echo $_SESSION['output']; ?>
-                    </div>
+                <!--CART-->
+                <div class = "footer">
+                    <h2 class = "center">Cart</h2>
                 </div>
             </div>
-            <!--CART-->
-            <div class = "footer">
-                <h2 class = "center">Cart</h2>
-            </div>
-        </div>
         </form>
     </body>
 </html>
